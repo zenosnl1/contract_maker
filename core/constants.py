d211dac64fd63ade2bae0c214362a498e0b5633d
zenosnl1 <ilya.milovzen@gmail.com>
2026-01-31 01:21:14 +0300

    Update constants.py

--- core/constants.py
+++ core/constants.py
@@ -1,1 +1,31 @@
 
+CONTRACT_TEMPLATE = "template_contract.docx"
+ACT_TEMPLATE = "template_act.docx"
+
+FIELDS = [
+    "FLAT_NUMBER",
+    "CLIENT_NAME",
+    "CLIENT_ID",
+"CLIENT_ADDRESS",
+"CLIENT_MAIL",
+"CLIENT_NUMBER",
+    "START_DATE",
+    "END_DATE",
+"CHECKOUT_TIME",
+"PRICE_PER_DAY",
+    "DEPOSIT",
+]
+
+QUESTIONS = {
+    "FLAT_NUMBER": "Номер помещения:",
+    "CLIENT_NAME": "Имя клиента:",
+    "CLIENT_ID": "Документ / персональный код:",
+"CLIENT_ADDRESS": "Адрес клиента:",
+"CLIENT_MAIL": "EMAIL клиента",
+"CLIENT_NUMBER": "Номер телефона клиента",
+    "START_DATE": "Дата заезда:",
+    "END_DATE": "Дата выезда:",
+"CHECKOUT_TIME": "Время выезда:",
+"PRICE_PER_DAY": "Цена за ночь:",
+    "DEPOSIT": "Депозит:",
+}
