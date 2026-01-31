42c926ea2afbea4d3a4460298fc06266815b9f4b
zenosnl1 <ilya.milovzen@gmail.com>
2026-02-01 02:40:25 +0300

    Update generate_contract_bot.py

--- generate_contract_bot.py
+++ generate_contract_bot.py
@@ -771,6 +771,7 @@ async def finalize_close(update, context):
         actual_checkout_date=context.user_data["actual_end_date"],
         returned_deposit=context.user_data["actual_returned_deposit"],
         deposit_comment=context.user_data.get("close_reason"),
+        "is_closed": True,
     )
 
     await update.message.reply_text(
@@ -884,6 +885,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 
