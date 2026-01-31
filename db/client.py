2d06a0c73af0399133c27bbdfd9222bbe760f98f
zenosnl1 <ilya.milovzen@gmail.com>
2026-02-01 00:27:31 +0300

    Update client.py

--- db/client.py
+++ db/client.py
@@ -1,6 +1,7 @@
 import os
 import requests
 from datetime import date
+from utils import build_contract_code
 
 SUPABASE_URL = os.environ["SUPABASE_URL"]
 SUPABASE_KEY = os.environ["SUPABASE_KEY"]
@@ -31,6 +32,55 @@ def fetch_active_contracts():
     )
     r.raise_for_status()
     return r.json()
+
+def save_contract_to_db(data, files):
+
+    url = os.environ["SUPABASE_URL"] + "/rest/v1/contracts"
+
+    headers = {
+        "apikey": os.environ["SUPABASE_KEY"],
+        "Authorization": f"Bearer {os.environ['SUPABASE_KEY']}",
+        "Content-Type": "application/json",
+        "Prefer": "return=minimal",
+    }
+
+    start = datetime.strptime(data["START_DATE"], "%d.%m.%Y")
+    end = datetime.strptime(data["END_DATE"], "%d.%m.%Y")
+
+    nights = (end - start).days
+    contract_code = build_contract_code(
+        data["START_DATE"],
+        data["FLAT_NUMBER"],
+    )
+
+    payload = {
+        "contract_code": contract_code,
+        "flat_number": data.get("FLAT_NUMBER"),
+
+        "client_name": data.get("CLIENT_NAME"),
+        "client_id": data.get("CLIENT_ID"),
+        "client_address": data.get("CLIENT_ADDRESS"),
+        "client_mail": data.get("CLIENT_MAIL"),
+        "client_number": data.get("CLIENT_NUMBER"),
+
+        "start_date": start.strftime("%Y-%m-%d"),
+        "end_date": end.strftime("%Y-%m-%d"),
+        "nights": nights,
+
+        "price_per_day": int(data["PRICE_PER_DAY"]),
+        "total_price": int(data["TOTAL_PRICE"]),
+        "deposit": int(data["DEPOSIT"]),
+
+        "checkout_time": data["CHECKOUT_TIME"],
+    }
+
+    r = requests.post(url, json=payload, headers=headers, timeout=10)
+
+    print("🟡 Supabase INSERT status:", r.status_code)
+    print("🟡 Supabase INSERT body:", r.text)
+    
+    if r.status_code not in (200, 201):
+        raise RuntimeError("Supabase insert failed")
 
 def insert_contract(payload):
     r = requests.post(
