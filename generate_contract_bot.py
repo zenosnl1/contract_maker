50669eeaa859c62c714b26b10c206fc8e74902bc
zenosnl1 <ilya.milovzen@gmail.com>
2026-03-10 01:31:59 +0300

    Update generate_contract_bot.py

--- generate_contract_bot.py
+++ generate_contract_bot.py
@@ -2875,12 +2875,12 @@ def main():
             ],
             
             FlowState.BOOKING_CREATE_START: [
-                CallbackQueryHandler(booking_date_callback, pattern="^DATE:")
+                CallbackQueryHandler(date_callback, pattern="^DATE:"),
             ],
             
             FlowState.BOOKING_CREATE_END: [
-                CallbackQueryHandler(booking_date_callback, pattern="^DATE:"),
-                CallbackQueryHandler(booking_end_unknown, pattern="^BOOKING_END_UNKNOWN$")
+                CallbackQueryHandler(date_callback, pattern="^DATE:"),
+                CallbackQueryHandler(booking_end_unknown, pattern="^BOOKING_END_UNKNOWN$"),
             ],
             
             FlowState.EXPENSES_MENU: [
@@ -2966,6 +2966,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 
